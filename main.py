cae8e4c3f66e840c5596df4305e3241bffb76ff2
SpiritTheWalf <161088035+SpiritTheWalf@users.noreply.github.com>
2024-02-27 19:16:35 +0000

    Added custom command prefix `%`

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 #main.py
 import os
 import discord
-from dotenv import load_dotenv
+from dotenv import  load_dotenv
+from discord.ext import commands
 
+
+load_dotenv()
+intents= discord.Intents.all()
 TOKEN = os.getenv('BOT_TOKEN')
+bot = commands.Bot(command_prefix= '%', intents=intents)
+
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connedted to Discord!')
+
+client.run(TOKEN)
