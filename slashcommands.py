a201ea4987e4696df264f01283ac8cdc225eb1c7
SpiritTheWalf <161088035+SpiritTheWalf@users.noreply.github.com>
2024-02-28 21:15:49 +0000

    removed database.py, timezones.db and timezones.py

--- slashcommands.py
+++ slashcommands.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import discord.app_commands
 from discord import app_commands
 from discord.ext import commands
-from pytz import timezone, all_timezones
+
 
 intents = discord.Intents.default()
 intents.members = True
